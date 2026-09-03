0141670f4a07819e058809eff2ff9e1f963e1d6e
Roman Marin <romanjesusmaringil@gmail.com>
2026-09-03 12:09:44 -0600

    UPDATE Unidad2

---
+++ Unidad1/Unidad2/03_for.py
@@ -0,0 +1,13 @@
+for numero in range(0,7, 2):
+    cuadrado = numero ** 2
+    print(numero, cuadrado)
+    materias = ["Python", "Linux", "Interfaces"] #trabajando for con rangos
+
+for materia in materias:
+    print(materia)
+    cadena = "0123456789ABCDEF"
+    for letra in cadena:
+        print (letra)
+
+for i in range (len(cadena)):
+    print(cadena[i])
---
+++ Unidad1/Unidad2/05_for1-100_primo.py
@@ -0,0 +1,6 @@
+for n in range(1, 101):
+    if n <= 1:
+        break
+else:
+        i = n + 1
+        print("Es primo")
